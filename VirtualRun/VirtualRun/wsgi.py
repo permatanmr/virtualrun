100998a32be9d6197d45f141192e4142bc6e2dcc
Software Engineering Prasmul <softwareengineeringprasmul@Softwares-MacBook-Pro.local>
2022-01-10 14:34:22 +0700

    add initial django project

---
+++ VirtualRun/VirtualRun/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for VirtualRun project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VirtualRun.settings')
+
+application = get_wsgi_application()
