99fa225d357f80209d0258506e83b06b20cb56fd
Software Engineering Prasmul <softwareengineeringprasmul@Softwares-MacBook-Pro.local>
2022-01-11 16:38:56 +0700

    add activities handler

--- VirtualRun/api/views.py
+++ VirtualRun/api/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def handleAuth(request):
-    code = request.GET.get('code', '')
+    #mengambil dari url parameter yang dilempar oleh Strava Auth system
+    code = request.GET.get('code', '')# <-- masukan ini ke DB untuk di simpen
+    print('CINTA')
     print(code)
+    #URL format https://www.strava.com/oauth/token?client_id=your_client_id&client_secret=your_client_secret&code=your_code_from_previous_step&grant_type=authorization_code
+    url_strava = 'https://www.strava.com/oauth/token?client_id=75955&client_secret=f76ea9ae9e60bcaabf96145bf2c7ae0f3d08a134&code='+code+'&grant_type=authorization_code'
+    x = requests.post(url_strava)
+    print('AKU PADAMU')# <-- data response yang diperoleh dari Strava
+    print(x.text)
+    responses = x.json()
+    access_token = responses['access_token']# <-- masukan ini ke DB untuk di simpen
+    refresh_token = responses['refresh_token']# <-- masukan ini ke DB untuk di simpen
+    print(access_token)
+
+    #URL format https://www.strava.com/api/v3/athlete/activities?access_token=access_token_from_previous_step
+    url_get_activities = 'https://www.strava.com/api/v3/athlete/activities?access_token='+access_token
+    x = requests.get(url_get_activities)# <-- masukan ini ke DB untuk di simpen
+    print('CINTAA LAGI')
+    print(x.text)
+    ## masukan ke DB
     content = ''
     if code == '':
         content = ''
